d677bc5cca4f061360f8f4ad5f8f384ed2d4c8d8
xrscode <120644276+xrscode@users.noreply.github.com>
2024-04-23 10:30:09 +0100

    Caesar cypher unfinished

--- katas/caesar_cipher.py
+++ katas/caesar_cipher.py
@@ -1,32 +1,35 @@
 import math
+
+
 def caesar(a, b):
     if not isinstance(a, str):
         raise TypeError('First argument must be a string!')
     elif not isinstance(b, int):
         raise TypeError('Second argument must be an integer!')
-    
-    cipher = ''
 
-    for char in a:
-        if char.isalpha():
-            print(ord(char), 'V')
-            new_val = ord(char) + b
-            print(new_val, 'x')
-            if new_val > 122:
-                multiple = math.floor(new_val / 57)
-                new_val = new_val - (multiple * 57) + 57
-                cipher += chr(new_val)
-            elif new_val < 65:
-                multiple = math.floor(new_val / 57 * -1)
-                new_val = new_val + (56 * multiple)
-                cipher += chr(new_val)
-            else:
-                cipher += chr(new_val)
-        else:
-            cipher += char
-    return cipher
+    new_str = ''
+
+    for letter in a:
+        if not letter.isalpha():
+            new_str += letter
+            continue
+
+        letter = ord(letter)
+        letter += b
+
+        if b == 0:
+            return a
+        elif b < 0:
+            multiple = math.ceil((letter - 122)/57)
+            c = letter + (58 * multiple)
+            new_str += chr(c)
+
+        elif b > 0:
+            multiple = math.ceil((letter - 122)/57)
+            c = letter - (58 * multiple)
+            new_str += chr(c)
+    return new_str
 
 
-
-print(caesar('A', -1))
-print(caesar('A', 1))
+print(caesar('z', -1), 'y')
+print(caesar('A', -1), 'z')
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,6 +1,7 @@
 from katas.caesar_cipher import caesar
 import pytest
 
+
 def test_first_argument_must_be_string():
     with pytest.raises(TypeError) as t:
         invoke = caesar(1, 1)
@@ -12,10 +13,57 @@ def test_second_argument_must_be_integer():
         invoke = caesar('a', 'a')
     assert str(t.value) == 'Second argument must be an integer!'
 
+
 def test_returns_a_string():
     invoke = caesar('a', 1)
     assert isinstance(invoke, str)
 
-def test_shifts_one_letter_by_one_place():
+
+def test_negative_shift_by_one_place():
+    invoke = caesar('A', -1)
+    assert invoke == 'z'
+
+
+def test_negative_shift_by_two_place():
+    invoke = caesar('A', -2)
+    assert invoke == 'y'
+
+
+def test_negative_shift_A_by_58_places():
+    invoke = caesar('A', -58)
+    assert invoke == 'A'
+
+
+def test_negative_A_shift_by_59_places():
+    invoke = caesar('A', -59)
+    assert invoke == 'z'
+
+
+def test_shifts_a_letter_by_one_place():
     invoke = caesar('a', 1)
     assert invoke == 'b'
+
+
+def test_shifts_z_letter_by_one_place():
+    invoke = caesar('z', 1)
+    assert invoke == 'A'
+
+
+def test_shifts_one_letter_by_one_57_places():
+    invoke = caesar('A', 57)
+    assert invoke == 'z'
+
+
+def test_shifts_one_letter_by_58_places():
+    invoke = caesar('A', 58)
+    assert invoke == 'A'
+
+
+def test_ignores_non_alphabetical_chars():
+    invoke = caesar('£ $%', 10)
+    assert invoke == '£ $%'
+
+
+def test_for_sentence_positive_displacement():
+    invoke = caesar('Hello World!', 10)
+    assert invoke == 'Rovvy ayBvn!'
