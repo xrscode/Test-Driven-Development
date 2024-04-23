33c724861a456842e1f007dbdb787e0e2af1436a
xrscode <120644276+xrscode@users.noreply.github.com>
2024-04-23 14:07:40 +0100

    Convert units completed

--- katas/convert_units.py
+++ katas/convert_units.py
@@ -1,22 +1,24 @@
+import math
+
+
 def convert(num):
     if not isinstance(num, int) or num <= 0:
         raise TypeError('Must be an integer larger than 1!')
 
-    start_division = int(1 * 10**(len(str(num)))/10)
-
     units = []
 
-    while num > 0:
-        a = int(num / start_division)
-        zero = ['0' for x in range(0, len(str(a)))]
-        units.append(f"{a} * 1{''.join(zero)}")
-        num -= a * int(zero[0])
-        try:
-            start_division = int(start_division/10)
-        except:
-            return ''.join(units)
+    while len(str(num)) > 1:
+        divisor = int(1*10**(len(str(num)))/10)
+        quantity = math.floor(num / divisor)
+        units.append(int(quantity * divisor))
+        num = num % divisor
 
-    return ''.join(units)
+    if num == 0:
+        return units
+    else:
+        units.append(num)
+
+    return units
 
 
-print(convert(90))
+print(convert(235))
--- tests/test_convert_units.py
+++ tests/test_convert_units.py
@@ -12,3 +12,33 @@ def test_only_accepts_integer_larger_than_1():
     with pytest.raises(TypeError) as t:
         invoke = convert(0)
     assert str(t.value) == 'Must be an integer larger than 1!'
+
+
+def test_returns_a_list():
+    invoke = convert(1)
+    assert isinstance(invoke, list)
+
+
+def test_returns_single_num_in_list():
+    invoke = convert(1)
+    assert invoke == [1]
+
+
+def test_returns_single_num_for_multiple_of_10():
+    invoke = convert(10)
+    assert invoke == [10]
+
+
+def test_returns_two_nums():
+    invoke = convert(11)
+    assert invoke == [10, 1]
+
+
+def test_returns_two_nums_for_101():
+    invoke = convert(101)
+    assert invoke == [100, 1]
+
+
+def test_returns_three_nums_for_115():
+    invoke = convert(115)
+    assert invoke == [100, 10, 5]
