b757f50b9000caf9fa468d2ae32cd57f38938aff
xrscode <120644276+xrscode@users.noreply.github.com>
2024-05-02 13:52:41 +0100

    Vowel shift completed

--- katas/vowel_shift.py
+++ katas/vowel_shift.py
@@ -1,2 +1,44 @@
-def shift():
-    pass
+def shift(string, integer):
+    """
+    Args:
+    String and integer. 
+
+    Returns:
+    String where all vowels shifted by integer amount. 
+
+    Hello, 1 >  Holle
+    """
+
+    if not isinstance(string, str):
+        raise TypeError('Argument 1 must be a string!')
+    elif not isinstance(integer, int) or isinstance(integer, bool):
+        raise TypeError('Argument 2 must be an integer!')
+    elif integer == 0:
+        return string
+
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    # Create list of vowels from string:
+    vowel_list = [x for x in string if x in vowels]
+
+    # Establish amount to shift by:
+    shift = integer
+    if integer >= len(vowel_list):
+        shift = integer % len(vowel_list)
+    elif integer == len(vowel_list):
+        shift = integer
+
+    # Shift the list of vowels by shift amount:
+    new = vowel_list[-shift:] + vowel_list[:-shift]
+
+    string = list(string)
+
+    return_str = ''
+    for x in string:
+        if x in vowels:
+            return_str += new[0]
+            new.pop(0)
+        else:
+            return_str += x
+
+    return ''.join(return_str)
--- tests/test_vowel_shift.py
+++ tests/test_vowel_shift.py
@@ -1,2 +1,54 @@
 from katas.vowel_shift import shift
 import pytest
+
+
+def test_returns_a_string():
+    assert isinstance(shift('hello', 1), str)
+
+
+def test_string_first_argument():
+    values = [True, False, None, [], {}, 1, 1.0]
+    for value in values:
+        with pytest.raises(TypeError) as t:
+            shift(value, 1)
+        assert str(
+            t.value) == 'Argument 1 must be a string!'
+    assert isinstance(shift('hello', 1), str)
+
+
+def test_int_first_argument():
+    values = [True, False, None, [], {}, 1.5, 1.0]
+    for value in values:
+        with pytest.raises(TypeError) as t:
+            shift('hello', value)
+        assert str(
+            t.value) == 'Argument 2 must be an integer!'
+    assert isinstance(shift('hello', 1), str)
+
+
+def test_return_string_if_integer_0():
+    assert shift('hello', 0) == 'hello'
+
+
+def test_shift_by_1():
+    assert shift('hello', 1) == 'holle'
+
+
+def test_shift_by_2():
+    assert shift('star nosed mole', 2) == 'stor nesad mole'
+
+
+def test_shift_by_4():
+    assert shift('funnily enough', 4) == 'finnely onuugh'
+
+
+def test_not_case_sensitive():
+    assert shift('fUnnIly EnOUgh', 4) == 'fInnEly OnUUgh'
+
+
+def test_negative_shift():
+    assert shift('hello', -1) == 'holle'
+
+
+def test_negative_shift_2():
+    assert shift('star nosed mole', -2) == 'ster nosed malo'
